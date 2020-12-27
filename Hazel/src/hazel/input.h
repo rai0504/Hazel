76b4e6360d4b7aaf97fc3b132e9f27edf2352dc0
Ryan <rai5395@gmail.com>
2020-12-27 15:06:30 +0200

    Added input polling

---
+++ Hazel/src/hazel/input.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "hazel/core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Input
+	{
+	public:
+		inline static bool isKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
+
+		inline static bool isMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
+		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
+		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
+		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) = 0;
+		
+		virtual bool IsMouseButtonPressedImpl(int button) = 0;
+		virtual std::pair<float, float> GetMousePositionImpl() = 0;
+		virtual float GetMouseXImpl() = 0;
+		virtual float GetMouseYImpl() = 0;
+	private:
+		static Input* s_Instance;
+	};
+
+}
